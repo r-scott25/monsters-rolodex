dd4b728dc6d2fe36251286d0cd398aa37f991f0a
r-scott25 <rachelbernal2@gmail.com>
2022-10-24 16:58:48 -0500

    added search box and functionality

--- src/App.js
+++ src/App.js
@@ -1,46 +1,69 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [ 
-        {
-          name: 'Linda',
-          id: '12e1231e',
-        },
-        {
-          name: 'Frank',
-          id: '12ed2dacas',
-        },
-        {
-          name: 'Jacky',
-          id: '1231e',
-        },
-        {
-          name: 'Andrei',
-          id: '12e1e213',
-        },
-     ],
+      monsters: [],
+      searchField: ''
     };
+    console.log("constructor");
   }
 
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+
+onSearchChange = (event) => {
+  console.log(event.target.value);
+  const searchField = event.target.value.toLocaleLowerCase();
+  this.setState(() => {
+    return { searchField };
+  });
+}
+
   render() {
+    console.log("render");
+
+    const { monsters, searchField} = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
-    <div className="App">
-      {
-        this.state.monsters.map((monster) => {
+      <div className="App">
+        <input 
+        className="search-box" 
+        type="search" 
+        placeholder="search monsters"
+        onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
         })}
-    </div>
+      </div>
     );
   }
 }
